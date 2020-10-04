f2ae4f1e6d9fd6b0c36f6975ad92ba19947878e5
Ezequiel <eolavaca@gmail.com>
2020-10-04 12:59:54 -0300

    portfolio starting

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -128,3 +128,16 @@
     });
 
 })();
+(function(){    
+    import Shuffle from 'shufflejs';
+    var Shuffle = window.Shuffle;
+    var element = document.querySelector('.my-shuffle-container');
+    var sizer = element.querySelector('.my-sizer-element');
+
+    var shuffleInstance = new Shuffle(element, {
+    itemSelector: '.picture-item',
+    sizer: sizer // could also be a selector: '.my-sizer-element'
+    });
+
+
+    })();
